71b468b2df25390c895ad3d928a3d1cee2764f47
glebBeliaev <flysolo.project@gmail.com>
2025-06-07 14:47:51 +0200

    Dz 1

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,22 @@
 package main
 
+import (
+	"log"
+
+	"github.com/glebbeliaev/purple_dz/config"
+	"github.com/glebbeliaev/purple_dz/internal/pages"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
+)
+
 func main() {
+	config.Init()
+	dbconfig := config.NewDataBaseConfig()
+	log.Println(dbconfig)
+	app := fiber.New()
+	app.Use(recover.New())
+
+	homeHandler := pages.NewHomeHandler(app)
+	homeHandler.Register()
+	app.Listen(":3000")
 }
--- internal/pages/handler.go
+++ internal/pages/handler.go
@@ -1,1 +1,19 @@
 package pages
+
+import "github.com/gofiber/fiber/v2"
+
+type HomeHandler struct {
+	router fiber.Router
+}
+
+func NewHomeHandler(router fiber.Router) *HomeHandler {
+	return &HomeHandler{
+		router: router,
+	}
+
+}
+func (h *HomeHandler) Register() {
+	h.router.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+}
