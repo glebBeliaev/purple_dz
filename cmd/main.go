68246078d8addc7829a8f9bfe3ab527eecde499d
glebBeliaev <flysolo.project@gmail.com>
2025-06-29 16:13:26 +0200

    start

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/glebbeliaev/purple_dz/config"
-	"github.com/glebbeliaev/purple_dz/internal/pages"
+	pages "github.com/glebbeliaev/purple_dz/internal/pages/home"
 	"github.com/glebbeliaev/purple_dz/pkg"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
